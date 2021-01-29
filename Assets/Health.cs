9dbf2debc5cd1b481f704fd9daab6dc26fd34845
SuperBrainBro <superbrainstormers@gmail.com>
2021-01-29 13:55:48 -0800

    hit stuff

---
+++ Assets/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Health : MonoBehaviour
+{
+    public AudioSource hitSound;
+    public Animator anim;
+
+    public int HP;
+
+    public float time;
+
+    public Text timeText;
+    public Text HPText;
+    void Start()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+        if (HP == 0)
+        {
+            //SceneManager.LoadScene("Boss");
+        }
+        time += .1f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Projectile")
+        {
+            anim.SetTrigger("Hit");
+            hitSound.Play();
+            HP -= 1;
+
+            timeText.text = time.ToString();
+            HPText.text = HP.ToString();
+        }
+    }
+}

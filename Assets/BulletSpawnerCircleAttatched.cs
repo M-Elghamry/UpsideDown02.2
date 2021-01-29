8f86c6fd7be334acac817343641fcfb68583061d
SuperBrainBro <superbrainstormers@gmail.com>
2021-01-28 21:11:46 -0800

    LOTS OF BOSS STUFF
    was just messing around, and this looks so sick!

--- Assets/BulletSpawnerCircleAttatched.cs
+++ Assets/BulletSpawnerCircleAttatched.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletSpawnerCircle : MonoBehaviour
+public class BulletSpawnerCircleAttatched : MonoBehaviour
 {
     public GameObject bullet;
     public float time;
@@ -16,7 +16,7 @@ public class BulletSpawnerCircle : MonoBehaviour
         transform.Rotate(new Vector3(0, 0, 1));
         if (timer <= 0)
         {
-            Instantiate(bullet, transform.position, Quaternion.identity);
+            Instantiate(bullet, transform, true);
             timer = time;
         }
         timer -= .01f;
